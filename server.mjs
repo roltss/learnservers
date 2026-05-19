0e91cdc7097970330f3422abc7e566f64b7ff7c3
Roland Taklai <roland.taklai@gmail.com>
2026-05-19 10:18:02 +0300

    Add node server dockerfile

--- server.mjs
+++ server.mjs
@@ -7,7 +7,7 @@ const server = createServer((req, res) => {
 });
 
 // starts a simple http server locally on port 3000
-server.listen(3000'127.0.0.1', () => {
+server.listen(3000'0.0.0.0', () => {
   console.log('Listening on 127.0.0.1:3000');
 });
 
